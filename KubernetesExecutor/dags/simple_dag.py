e8bb705e5f627bcc3678d087dbe66dee94ab40af
Rangdog <thanhhaxuan02@gmail.com>
2025-06-07 14:03:52 +0700

    Kubernetes executor

---
+++ KubernetesExecutor/dags/simple_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='hello_k8s',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["demo"],
+) as dag:
+    t1 = BashOperator(
+        task_id="say_hello",
+        bash_command="echo Hello from KubernetesExecutor!",
+    )
